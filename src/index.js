79ba464f2031bcf0fdb21470db0c387ca62db838
Marcin Podgorski <marcinpodgorski@MacBook-Pro-Marcin.local>
2023-01-26 17:11:35 +0100

    create new branch

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+  <div>
     <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
   </React.StrictMode>
 );
 
