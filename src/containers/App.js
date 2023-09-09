c917b9f91d7131a1c08239343f4d590d1f092c01
marcinpodgorski <podgorski.marcin93@gmail.com>
2023-09-09 12:49:08 +0200

    adding folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                this.setState({ robots: users})
+            });
     }
 
     onSearchChange = (event) => {
@@ -18,8 +27,9 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
         return (
             <div className='tc'>
